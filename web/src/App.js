012b5c5bfd955b3c1eda61add3d2fc5c791f2af3
francisco-quintana <francisco.quintanas12@gmail.com>
2020-02-09 11:58:40 -0700

    uffa el frankenstein

--- web/src/App.js
+++ web/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Parquiem</h1>
-    </div>
+<div className="text-center">
+  <header class="bg-blue-900 m-6 p-6 rounded shadow-lg ">
+    <img src="logo.png"  class="App-logo contente-center m-0 p-6 inline-block" alt="logo"/>
+    <h1 class="text-white text-3xl">Hola ReactWindCSS</h1>
+  </header>
+</div>
   );
 }
 
