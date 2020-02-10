5951211a841d33b43abada4094852ddd12018c6a
a324603 <asaelcerros@gmail.com>
2020-02-09 20:49:33 -0700

    Se borran keys

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://parking-admin:tFvOSKUDd574NepT@parquiem-cluster-ryeno.gcp.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-};
