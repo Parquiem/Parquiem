b338784fdb094fff22117a1ad080b9eb2789de78
Carlos Ortega <the_king_00carlos@hotmail.com>
2020-02-18 09:45:44 -0700

    Parquimetro API

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
+const parquimetro = require('./routes/api/parquimetro');
+
 
 const app = express();// Bodyparser middleware
 app.use(
@@ -30,6 +32,7 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+app.use("/api/parquimetro", parquimetro);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
