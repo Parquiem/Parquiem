9c5b7b3c5dc4654f57cfdbe3fe2d944529aad961
francisco-quintana <francisco.quintanas12@gmail.com>
2020-02-24 21:15:28 -0700

    intento de sign up

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
  
 import Home from './Home';
 import Login from './Login';
+import Signup from './Signup';
+import Signup2 from './Signup2';
+import Signup3 from './Signup3';
  
 class App extends Component {
   render() {
@@ -12,6 +15,9 @@ class App extends Component {
             <Switch>
              <Route path="/" component={Home} exact/>
              <Route path="/login" component={Login}/>
+             <Route path="/signup" component={Signup}/>
+             <Route path="/signup2" component={Signup2}/>
+             <Route path="/signup3" component={Signup3}/>
            </Switch>
         </div> 
       </BrowserRouter>
