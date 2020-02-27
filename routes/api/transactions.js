70acce7471cf325d6b2597dcd2d00de6835fa2e5
a324603 <asaelcerros@gmail.com>
2020-02-26 21:53:30 -0700

    purchases

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -5,7 +5,19 @@ const keys = require("../../config/keys");
 const User = require("../../models/User");
 
 
-// @route POST api/transactions/add
+// @route POST api/transactions/add/:id
 // @desc Registers a transaction
 // @access Private
+router.post('/add/:id', (req, res) => {
+    let id = req.params.id;
+    const newTransc = {
+        cost : req.body.cost,
+        lat : req.body.lat,
+        lng : req.body.lng
+    }
+    User.findByIdAndUpdate(id, {$push: {purchases: newTransc}})
+        .then(() => res.json({ success: true, newTransc}))
+        .catch(err => res.status(404).json({ success: false, err}))
+});
 
+module.exports = router;
