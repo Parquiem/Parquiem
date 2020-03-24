9d600309b40ab90dc077360c45f11b75131eed58
francisco-quintana <francisco.quintanas12@gmail.com>
2020-03-24 11:27:16 -0700

    Pagina acabada, solo sin conexión a sensores

--- web/src/Home.js
+++ web/src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from './Navigation'
-
+import './css/home.css';
+import {NavLink} from 'react-router-dom';
 function Home() {
 
   return (
@@ -14,18 +15,19 @@ function Home() {
       </div>
       <img src="images/page1.png" class="md:w-1/2 lg:w-1/3 content-center ml-auto mr-auto block mt-1" alt="imagen parquiem"></img>
     </div>
-    {/* <div class="page2">
-        <div class="container relative float-right">
-        <div class="textcontain absolute">
-        <p class="text-3xl font-serif text-right">Paga desde tu celular</p>
-        <p class="text-xl text-xl font-serif text-right">Con la aplicación puedes pagar el parquímetro </p>
-        <p class="text-xl text-xl font-serif text-right">que estas ocupando desde cualquier lugar</p>
-        </div>
-        </div>
-        <img src="Vector5.png"></img>
-      </div> */}
+    <div class="page2">
+      <img src="images/data.png" class="w-screen h-auto" alt="data"></img>
     </div>
-  </div>
+    <div class="page3 bg-cover w-full text-center" id="rectangulo">
+      <p class="align-center font-serif text-white text-3xl pt-24">Crea tu cuenta</p>
+      <p class="align-center font-serif text-white text-xl pt-5 pb-20">Se parte del futuro de los parquimetros</p>
+      <NavLink to="/Signup"><button class="outline-none border-white border-2 text-2xl text-white bg-transparent pl-10 pr-10 pt-2 pb-2 rounded-full font-serif">Inscribete</button></NavLink>
+      </div>
+      <div class="bg-gray-900 w-full flex justify-center items-center" id="footer">
+        <p class="font-serif font-hairline text-gray-600 text-sm text-center align-middle">Designed by Parquiem Team in Chihuahua</p>
+      </div>
+    </div>
+    </div>
   );
 }
 export default Home;
--- web/src/map/Map.js
+++ web/src/map/Map.js
@@ -7,13 +7,19 @@ import {
 } from 'react-google-maps'
 
 const Map= (props)=>{
-    const color="blue-dot"
+    const red="red-dot"
+    const green="green-dot"
     return(
         <GoogleMap 
         defaultZoom={18}
         defaultCenter={{lat: 28.639990, lng:-106.072882}}
         >
-        <Marker position={{ lat: 28.639990, lng:-106.072882}} icon={`http://maps.google.com/mapfiles/ms/icons/${color}.png`}/>
+        <Marker position={{ lat: 28.640220, lng:-106.072882}} icon={`http://maps.google.com/mapfiles/ms/icons/${red}.png`}/>
+        <Marker position={{ lat: 28.640280, lng:-106.072782}} icon={`http://maps.google.com/mapfiles/ms/icons/${red}.png`}/>
+        <Marker position={{ lat: 28.640280, lng:-106.072582}} icon={`http://maps.google.com/mapfiles/ms/icons/${green}.png`}/>
+        <Marker position={{ lat: 28.640100, lng:-106.072982}} icon={`http://maps.google.com/mapfiles/ms/icons/${green}.png`}/>
+        <Marker position={{ lat: 28.640050, lng:-106.073022}} icon={`http://maps.google.com/mapfiles/ms/icons/${red}.png`}/>
+        <Marker position={{ lat: 28.639982, lng:-106.072102}} icon={`http://maps.google.com/mapfiles/ms/icons/${green}.png`}/>
         </GoogleMap>
     );
 };
